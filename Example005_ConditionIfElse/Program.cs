99cc3ae332d81f6002bc152391b656b786a880aa
Madina <madinanazarovaa@gmail.com>
2022-12-14 23:20:06 +0600

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿string username = Console.ReadLine();
+
+if(username.ToLower() == "мадина")
+{
+    Console.WriteLine("Ура, это же МАДИНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
